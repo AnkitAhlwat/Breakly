73a354f5dc97b97729e97195aca00d3b42fa94d9
Cassidy Boilley <cboilley@my.bcit.ca>
2022-11-07 22:57:18 -0800

    add next reminder calculation

--- scripts/calculateintervals.js
+++ scripts/calculateintervals.js
@@ -3,12 +3,52 @@
 ///right after pressing the submit button on set reminder page
 var time = db.collections('users').doc('user.uid')
 time.get(reminders['start_time']).then(function (doc) {
-    $('#break_time').html(doc.data().start_time);
+  
+  $('#break_time').html(doc.data().start_time);
 
-    ///after pressing the "I've taken my break" button
-    var time = db.collections('users').doc('user.uid')
-    var start_time = time.get(reminders['start_time']).then()
-    var interval = time.get(reminders['interval']).then()
-    var new_break_time = start_time + interval
-    time.update(reminders['start_time'], new_break_time)
+  var break_intervals = [];
+  var next_break = ""
+  var start_time = "20:30"
+  var end_time = "23:30"
+  var interval = "30"
+  var startDate = new Date("1/1/2015 " + start_time);
+  var endDate = new Date("1/1/2015 " + end_time);
+  var offset = interval * 1000 * 60;
+  var current_time = new Date();
+  var current_time = current_time.getHours() + ":" + current_time.getMinutes();
+  current_time = new Date("1/1/2015 " + current_time);
+  console.log(current_time)
+
+  do {
+      startDate = new Date(startDate.getTime() + offset);
+      
+          break_intervals.push(startDate);
+  } while (startDate < endDate);
+  console.log(break_intervals);
+  console.log(break_intervals[4])
+
+  for(i = 0; i < break_intervals.length; i++) {
+      if (break_intervals[i] > current_time) {
+
+        next_break = Math.abs(break_intervals[i] - current_time);
+        console.log(next_break = Math.floor((next_break / (1000 * 60)) % 60));
+        document.getElementById("break_time").innerHTML
+        
+
+      }
+
+
+  }
+  // this should create an array of time intervals, then will compare the compare to the next closest interval and then calculate the remaining until the next reminder
 })
+
+  
+//     you essentially need to pull their start time
+// and then everytime they log a break
+// add their increment value to their start time
+// store it as their new start time
+// until start time == end time
+
+    
+    
+
