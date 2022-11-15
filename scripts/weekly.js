40de160240bafcd83ed86afb7d2e4ef62b0450b9
ankitahlwat <ankitahlwat54@gmail.com>
2022-11-15 09:51:50 -0800

    Add weekly.js and read data from Firestore

---
+++ scripts/weekly.js
@@ -0,0 +1,44 @@
+
+var user = firebase.auth().currentUser;
+
+
+function goalTracker() {
+    console.log("ready")
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+
+
+        if (user) {
+            db.collection("users").get()
+
+                .then(snap => {
+                    snap.forEach(element => {
+                        console.log(element.id)
+                        console.log(user.uid)
+                        if (element.id == user.uid) {
+                            const user_name = element.data().name
+                            const break_counter = element.data().break_counter
+                            const personal_goal = element.data().personal_goal
+                            const goal = (break_counter / personal_goal) * 100
+                            $("#name").html(user_name)
+                            // Learn how to get data from a time span
+                            // $("#c_weekly_goal").html(break_counter)
+                            // $("#p_weekly_goal").html(personal_goal)
+                            // $("#weekly_goal").html(goal)
+                            console.log(element)
+                            console.log(goal)
+
+                        }
+                    });
+
+                }
+                )
+
+
+        }
+
+    }
+    );
+}
+
+goalTracker();
