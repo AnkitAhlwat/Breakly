c7aed092e4e7486e477b495a0070045e270e00d6
ankitahlwat <ankitahlwat54@gmail.com>
2022-11-17 12:40:06 -0800

    Polish file for readability

--- scripts/weekly.js
+++ scripts/weekly.js
@@ -12,15 +12,11 @@ function goalTracker() {
 
                 .then(snap => {
                     snap.forEach(element => {
-                        // console.log(element.id)
-                        // console.log(user.uid)
+
                         if (element.id == user.uid) {
                             var today = new Date().getDay();
-                            const user_name = element.data().name
                             var break_counter = Number(element.data().break_counter)
                             var personal_goal = Number(element.data().personal_goal)
-                            const goal = Number((break_counter / personal_goal) * 100)
-                            $("#name").html(user_name)
                             currentUser = db.collection("users").doc(user.uid)
                             currentUser.set({
                                 days_breaks_taken: { [today]: break_counter }
@@ -33,12 +29,6 @@ function goalTracker() {
                                 merge: true
                             })
 
-                            // Learn how to get data from a time span
-                            // $("#c_weekly").html(break_counter)
-                            // $("#p_weekly").html(personal_goal)
-                            // $("#goal_progress").html(goal)
-                            console.log(element)
-                            console.log(goal)
 
 
                         }
@@ -55,99 +45,4 @@ function goalTracker() {
     );
 
 }
-// goalTracker();
-// setDailyGoal();
-
-var today = new Date().getDay();
-// console.log(today)
-// console.log(currentUser)
-// function setDailyGoal(breaks_taken) {
-//     if (today == 1)
-//         currentUser.set({
-//             days: [{ breaks_taken: break_counter }]
-//         }, {
-//             merge: true
-//         })
-//     if (today == 2)
-//         currentUser.set({
-//             days: [{ breaks_taken: break_counter }]
-//         }, {
-//             merge: true
-//         })
-//     if (today == 3)
-//         currentUser.set({
-//             monday: [{ breaks_taken: break_counter }]
-
-//         }, {
-//             merge: true
-//         })
-//     if (today == 4)
-//         currentUser.set({
-//             tuesday: 7
-//         }, {
-//             merge: true
-//         })
-//     if (today == 5)
-//         currentUser.set({
-//             days: [{ breaks_taken: break_counter }]
-//         }, {
-//             merge: true
-//         })
-//     if (today == 6)
-//         currentUser.set({
-//             days: [{ breaks_taken: break_counter }]
-//         }, {
-//             merge: true
-//         })
-//     if (today == 7)
-//         currentUser.set({
-//             days: [{ breaks_taken: break_counter }]
-//         }, {
-//             merge: true
-//         })
-
-// }
-
-//-----------------------------------------------------------------------------
-// This function is called whenever the user clicks on the "bookmark" icon.
-// It adds the hike to the "bookmarks" array
-// Then it will change the bookmark icon from the hollow to the solid version. 
-//-----------------------------------------------------------------------------
-function saveBookmark() {
-    currentUser.set({
-        bookmarks: 7
-    }, {
-        merge: true
-    })
-        .then(function () {
-            console.log("bookmark has been saved for: " + currentUser);
-            var iconID = 'save-' + hikeID;
-            //console.log(iconID);
-            //this is to change the icon of the hike that was saved to "filled"
-            document.getElementById(iconID).innerText = 'bookmark';
-        });
-}
-// console.log(break_counter)
-// saveBookmark();
-
-// function goalTracker() {
-//     console.log("ready")
-//     firebase.auth().onAuthStateChanged(user => {
-//         // Check if a user is signed in:
-
-
-//         if (user) {
-//             currentUser = db.collection("users").doc(user.uid)
-//             currentUser.set({
-//                 tuesday: [{ breaks_taken: break_counter }]
-//             }, {
-//                 merge: true
-//             })
-
-
-
-//         }
-//     }
-//     )
-// }
 goalTracker();
