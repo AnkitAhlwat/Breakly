c619837c6e2bb130f5791e4a462c076fa6a85d67
Cassidy Boilley <cboilley@my.bcit.ca>
2022-11-23 14:22:47 -0800

    change stuff for profile

--- scripts/getUserData.js
+++ scripts/getUserData.js
@@ -6,8 +6,14 @@ function getUserData(){
             currentUser = db.collection("users").doc(user.uid)
             currentUser.get()
                 .then(userDoc => {
-                
-                
+                    let picUrl = userDoc.data().profilePic;
+                    
+                    console.log(picUrl);
+                                
+                    $("#mypic-goes-here").attr("src", picUrl);
+                        
+                    
+                    
                     document.getElementById("name").innerHTML = userDoc.data().name
                     console.log(userDoc.data().name)
                     document.getElementById("school").innerHTML = userDoc.data().school
@@ -16,8 +22,7 @@ function getUserData(){
                     console.log(userDoc.data().email)
                     document.getElementById("break_counter").innerHTML = userDoc.data().break_counter
                     console.log(userDoc.data().break_counter)
-                    document.getElementById("mypic-goes-here").innerHTML = userDoc.data().profilePic
-                    console.log(userDoc.data().profilePic)
+                    
 
             })
         }
