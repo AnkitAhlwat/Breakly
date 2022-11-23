4d176a38de3c8d57193a12671858960d971cea3d
ankitahlwat <ankitahlwat54@gmail.com>
2022-11-23 14:34:49 -0800

    Add progress bar for each day

--- scripts/weekly.js
+++ scripts/weekly.js
@@ -11,17 +11,30 @@ function goalTracker() {
                 var goal_total = 0
                 for (var key in array_of_personal_goals) {
                     goal_total += array_of_personal_goals[key];
-
                 };
                 var break_total = 0
                 for (var key in array_of_breaks_taken) {
                     break_total += array_of_breaks_taken[key];
 
                 };
-                var progress = (break_total / goal_total) * 100
+                var progress = (break_total / goal_total) * 100;
+                for (i = 1; i <= Object.keys(array_of_breaks_taken).length; i++) {
+                    console.log(i)
+                    width = (array_of_breaks_taken[i] / array_of_personal_goals[i]) * 100
+                    $(".container").append(
+                        `
+
+                        <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-label="Example with label" style="width: ${width}% ;" aria-valuenow="${width}" aria-valuemin="0" aria-valuemax="100]">${width}%</div>
+                        </div>
+                        <br>
+                        `
+                    )
+                }
                 $("#breaks").html(break_total);
                 $("#personal_goal").html(goal_total);
                 $("#progress").html(progress.toFixed());
+
 
             })
         }
