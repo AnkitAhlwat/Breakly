40e16aec5db0cefc2d7e0c035fe1bc37057a7231
ankitahlwat <ankitahlwat54@gmail.com>
2022-11-22 20:24:31 -0800

    Add check to see if logged in for weekly summary

---
+++ scripts/footer.js
@@ -0,0 +1,24 @@
+var user = firebase.auth().currentUser;
+
+function write_data() {
+
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            window.location.href = "weekly.html";
+            console.log("yo")
+        }
+        else {
+            window.location.href = "login_homepage.html"
+        }
+    })
+}
+
+
+function setup() {
+    $('#weekly').on("click", function () {
+        write_data()
+    });
+}
+
+$(document).ready(setup);
