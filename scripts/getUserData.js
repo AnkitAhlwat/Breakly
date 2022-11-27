4f99a39e16c17a2e4db40b394e0ad537d63a95ad
ankitahlwat <ankitahlwat54@gmail.com>
2022-11-26 16:19:03 -0800

    Update console.log for readability

--- scripts/getUserData.js
+++ scripts/getUserData.js
@@ -1,5 +1,5 @@
-function getUserData(){
-            
+function getUserData() {
+
     firebase.auth().onAuthStateChanged(user => {
 
         if (user) {
@@ -7,24 +7,24 @@ function getUserData(){
             currentUser.get()
                 .then(userDoc => {
                     let picUrl = userDoc.data().profilePic;
-                    
-                    console.log(picUrl);
-                                
-                    $("#mypic-goes-here").attr("src", picUrl);
-                        
-                    
-                    
-                    document.getElementById("name").innerHTML = userDoc.data().name
-                    console.log(userDoc.data().name)
-                    document.getElementById("school").innerHTML = userDoc.data().school
-                    console.log(userDoc.data().school)
-                    document.getElementById("email").innerHTML = userDoc.data().email
-                    console.log(userDoc.data().email)
-                    document.getElementById("break_counter").innerHTML = userDoc.data().break_counter
-                    console.log(userDoc.data().break_counter)
-                    
 
-            })
+                    console.log("The pic url is", picUrl);
+
+                    $("#mypic-goes-here").attr("src", picUrl);
+
+
+
+                    document.getElementById("name").innerHTML = userDoc.data().name
+                    console.log("The user's name is:", userDoc.data().name)
+                    document.getElementById("school").innerHTML = userDoc.data().school
+                    console.log("The user's school is:", userDoc.data().school)
+                    document.getElementById("email").innerHTML = userDoc.data().email
+                    console.log("The user's email is:", userDoc.data().email)
+                    document.getElementById("break_counter").innerHTML = userDoc.data().break_counter
+                    console.log("The user's current break counter is at:", userDoc.data().break_counter)
+
+
+                })
         }
     });
 }
