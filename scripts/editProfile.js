786592337dc4d583d713c7c5f150164a617c2ad0
hsu-patty <pattyhsu615@gmail.com>
2022-11-28 22:34:08 -0800

    Fix spacing in editProfile.js

--- scripts/editProfile.js
+++ scripts/editProfile.js
@@ -1,15 +1,15 @@
 var ImageFile;      //global variable to store the File Object reference
 
-function chooseFileListener(){
+function chooseFileListener() {
     const fileInput = document.getElementById("mypic-input");   // pointer #1
     const image = document.getElementById("mypic-goes-here");   // pointer #2
 
     //attach listener to input file
     //when this file changes, do something
-    fileInput.addEventListener('change', function(e){
+    fileInput.addEventListener('change', function (e) {
 
         //the change event returns a file "e.target.files[0]"
-	      ImageFile = e.target.files[0];
+        ImageFile = e.target.files[0];
         var blob = URL.createObjectURL(ImageFile);
 
         //change the DOM img element source to point to this file
@@ -31,7 +31,7 @@ function saveUserInfo() {
                 storageRef.getDownloadURL()
                     .then(function (url) { // Get "url" of the uploaded file
                         console.log("Got the download URL.");
-												//get values from the from
+                        //get values from the form and store in relevant variables
                         userName = document.getElementById('nameInput').value;
                         userSchool = document.getElementById('schoolInput').value;
                         userCountry = document.getElementById('countryInput').value;
@@ -40,13 +40,13 @@ function saveUserInfo() {
 
                         //Asynch call to save the form fields into Firestore.
                         db.collection("users").doc(user.uid).update({
-                                name: userName,
-                                school: userSchool,
-                                country: userCountry,
-                                email: userEmail,
-                                aboutme: useraboutme,
-                                profilePic: url // Save the URL into users collection
-                            })
+                            name: userName,
+                            school: userSchool,
+                            country: userCountry,
+                            email: userEmail,
+                            aboutme: useraboutme,
+                            profilePic: url // Save the URL into users collection
+                        })
                             .then(function () {
                                 console.log('Added Profile Pic URL to Firestore.');
                                 console.log('Saved user profile info');
@@ -58,63 +58,63 @@ function saveUserInfo() {
 }
 
 function editUserInfo() {
-   //Enable the form fields
-   document.getElementById('personalInfoFields').disabled = false;
+    //Enable the form fields
+    document.getElementById('personalInfoFields').disabled = false;
 }
 
 function populateInfo() {
     firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                // go and get the logged in user's info from firestore
-                currentUser = db.collection("users").doc(user.uid);
+        if (user) {
+            // go and get the logged in user's info from firestore
+            currentUser = db.collection("users").doc(user.uid);
 
-                currentUser.get()
-                    .then(userDoc => {
-                        let userName = userDoc.data().name;
-                        console.log(userName)
-                        let userSchool = userDoc.data().school;
-                        console.log(userSchool)
-                        let userCountry = userDoc.data().country;
+            currentUser.get()
+                .then(userDoc => {
+                    let userName = userDoc.data().name;
+                    console.log(userName)
+                    let userSchool = userDoc.data().school;
+                    console.log(userSchool)
+                    let userCountry = userDoc.data().country;
+                    console.log(userCountry)
+                    let userEmail = userDoc.data().email;
+                    console.log(userEmail)
+                    let userAboutMe = userDoc.data().aboutme;
+                    console.log(userAboutMe)
+                    let picUrl = userDoc.data().profilePic;
+                    console.log(picUrl)
+                    // assign the user info into the appropriate id's
+                    if (userName != null) {
+                        document.getElementById("nameInput").value = userName;
+                    }
+                    if (userSchool != null) {
+                        document.getElementById("schoolInput").value = userSchool;
+                    }
+                    if (userCountry != null) {
                         console.log(userCountry)
-                        let userEmail = userDoc.data().email;
+                        document.getElementById("countryInput").value = userCountry;
+                    }
+                    if (userEmailer != null) {
                         console.log(userEmail)
-                        let userAboutMe = userDoc.data().aboutme;
+                        document.getElementById("emailInput").value = userEmail;
+                    }
+                    if (userAboutMe != null) {
                         console.log(userAboutMe)
-                        let picUrl = userDoc.data().profilePic;
-                        console.log(picUrl)
-                        // assign the user info into the appropriate id's
-                        if (userName != null){
-                            document.getElementById("nameInput").value = userName;
-                        }
-                        if (userSchool != null){
-                            document.getElementById("schoolInput").value = userSchool;
-                        }
-                        if (userCountry != null){
-                            console.log(userCountry)
-                            document.getElementById("countryInput").value = userCountry;
-                        }
-                        if (userEmailer != null){
-                            console.log(userEmail)
-                            document.getElementById("emailInput").value = userEmail;
-                        }
-                        if (userAboutMe != null){
-                            console.log(userAboutMe)
-                            document.getElementById("aboutmeInput").value = userAboutMe;
-                    
-                        }
-                        if (picUrl != null){
-                            console.log(picUrl);
-								        
-                            $("#mypic-goes-here").attr("src", picUrl);
-                        }
-                        else
-                        console.log("picURL is null");
-                    })
+                        document.getElementById("aboutmeInput").value = userAboutMe;
 
-            } else {
-                console.log("no user is logged in")
-            }
+                    }
+                    if (picUrl != null) {
+                        console.log(picUrl);
+
+                        $("#mypic-goes-here").attr("src", picUrl);
+                    }
+                    else
+                        console.log("picURL is null");
+                })
+
+        } else {
+            console.log("no user is logged in")
         }
+    }
 
     )
 
