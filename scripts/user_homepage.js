b568d19d56013a6027ca2b0482f1f5571a58cbe7
ankitahlwat <ankitahlwat54@gmail.com>
2022-11-29 20:19:14 -0800

    Add write_firebase() function

--- scripts/user_homepage.js
+++ scripts/user_homepage.js
@@ -1,5 +1,33 @@
 var user = firebase.auth().currentUser;
 
+// Write data to firebase to update new breaks_counter and personal_goal
+function write_firebase() {
+    firebase.auth().onAuthStateChanged(user => {
+
+        if (user) {
+            db.collection("users").doc(user.uid).onSnapshot(function (doc) {
+                var today = new Date().getDay();
+                var break_counter = doc.data().break_counter;
+                var personal_goal = doc.data().personal_goal;
+
+                // Update the days_breaks_taken/days_personal_goal arrays in firebase based on the day
+                // If no data exists, it will set the data for the first time
+                currentUser.set({
+                    days_breaks_taken: { [today]: break_counter }
+                }, {
+                    merge: true
+                })
+                currentUser.set({
+                    days_personal_goal: { [today]: personal_goal }
+                }, {
+                    merge: true
+                })
+            })
+        }
+    })
+
+}
+
 //function to log the break counter when the user clicks the button
 function log_break_counter() {
     firebase.auth().onAuthStateChanged(user => {
@@ -23,25 +51,12 @@ function picture_setting() {
 
             //Read the data from firestore and stores it in a variable to update user homepage with this data
             db.collection("users").doc(user.uid).onSnapshot(function (doc) {
-                var today = new Date().getDay();
                 var break_counter = doc.data().break_counter;
                 var personal_goal = doc.data().personal_goal;
                 var progress = (break_counter / personal_goal)
                 $("#breaks").html(break_counter);
                 $("#personal_goal").html(personal_goal);
 
-                // Update the days_breaks_taken/days_personal_goal arrays in firebase based on the day
-                // If no data exists, it will set the data for the first time
-                currentUser.set({
-                    days_breaks_taken: { [today]: break_counter }
-                }, {
-                    merge: true
-                })
-                currentUser.set({
-                    days_personal_goal: { [today]: personal_goal }
-                }, {
-                    merge: true
-                })
 
                 //Calculate the user progress towards their goal, and sets different plant image depending on progress
                 if (progress < 0.25) {
@@ -56,6 +71,8 @@ function picture_setting() {
                 else {
                     document.getElementById("picture").src = "./images/plant_4.jpg";
                 }
+                // Call write_firebase() to write new firebase data 
+                write_firebase()
             })
         }
     })
